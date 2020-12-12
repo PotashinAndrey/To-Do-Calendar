4c84c75969cb00e60dc44c8b7764cb2b42e698f9
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2020-12-13 02:24:21 +0500

    убраны старые завитсимости

--- src/modules/aboutEvent/ChooseChildren.jsx
+++ src/modules/aboutEvent/ChooseChildren.jsx
@@ -1,21 +1,19 @@
 import Raect, { useState, useEffect } from 'react';
 import SearchLine from '../toDoList/toDoComponents/SearchLine.jsx';
-import List from '../toDoList/toDoComponents/List.jsx'
 import NoteList from '../toDoList/NoteList/NoteList.jsx';
-// import useFilterContext from '../Contexts/FilterContext.jsx';
-// import useNoteContext from '../Contexts/NoteContext.jsx';
+import useFiltersContext from '../Contexts/FiltersContext.jsx';
+import useCurrentNoteContext from '../Contexts/CurrentNoteContext.jsx';
 import './ChooseChildren.css';
 
 export default function ChooseChildren() {
-  const { noteState } = useNoteContext();
-  const { filterState, filterDispatch } = useFilterContext();
+  const { filtersState, filtersDispatch } = useFiltersContext();
+  const { currentNoteState, currentNoteDispatch } = useCurrentNoteContext();
 
 
   const [findLine, setFindLine] = useState('');
 
   useEffect(() => {
-    filterDispatch({
-      notes: noteState.notes,
+    filtersDispatch({
       filters: {
         name: findLine
       }
@@ -25,8 +23,10 @@ export default function ChooseChildren() {
   return (
     <div>
       <SearchLine findLine={findLine} setFindLine={setFindLine} />
-      {/* <List style={{height: '300px'}} /> */}
-      <NoteList list={filterState} >
+      <NoteList
+        list={filtersState}
+        onClick={note => currentNoteDispatch({ currentNote: {children: [...currentNoteState.currentNote.children, note.id]} })}
+      >
 
       </NoteList>
     </div>
